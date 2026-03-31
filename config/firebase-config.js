5aca31ccd156494054588b9b755e7cce6d12a921
PracticasEstadias26 <rafhasoluciones26@gmail.com>
2026-03-31 16:15:09 -0600

    configuración de lineko a base

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase
 
 // Configuracion de firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB5D45RI21rRAJB9mlt1NeI6N4d3PDyEqg",
-  authDomain: "centinela-mx.firebaseapp.com",
-  projectId: "centinela-mx",
-  storageBucket: "centinela-mx.firebasestorage.app",
-  messagingSenderId: "215358382201",
-  appId: "1:215358382201:web:cf7e0fffb00ca36b05b4a8",
-  measurementId: "G-0S5PVJX04Y"
+  apiKey: "AIzaSyAkRF8CUBs8dKlpk6N2U4EhESYhsOAW9sQ",
+  authDomain: "centinela-c5.firebaseapp.com",
+  databaseURL: "https://centinela-c5-default-rtdb.firebaseio.com",
+  projectId: "centinela-c5",
+  storageBucket: "centinela-c5.firebasestorage.app",
+  messagingSenderId: "572612004113",
+  appId: "1:572612004113:web:9ff187d18068d1fdf5a4b0",
+  measurementId: "G-GP0ZLWBT7Y",
 };
 
 // Initialize Firebase
